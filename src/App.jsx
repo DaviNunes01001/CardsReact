76ddc11970e93f2e082d1dd42bd8d376ebf2130f
hakizao6074 <davizinhodjar@gmail.com>
2026-03-24 10:18:21 -0300

    alteração do card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import Card from './components/Card/Card'
+import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
- 
-
   return (
-    <>
-    <div className='Container'>
+    <div className="Container">
       <Card
-      Titulo="O que são props?"
-      desc="Props são dados passados de um componente para outro, permitindo que ele mostre informações diferentes."
-      code={`<Card titulo="Olá mundo" />`}
-      /> </div>
-    </>
-  )
+        Titulo="O que são props?"
+        desc="Props são dados passados de um componente para outro, permitindo que ele mostre informações diferentes."
+        code={`<Card titulo="Olá mundo" />`}
+      />
+      <Card
+        Titulo="Passando props para componentes"
+        desc="Props são como parâmetros que você envia para um componente."
+        code={`<Card titulo="Olá" desc="Isso é um card"/>`}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
