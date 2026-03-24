1c3dd0c5d4ae3ee43f8032d065184b5446530d02
sevenstoorm <miguel92.bf@gmail.com>
2026-03-24 10:16:01 -0300

    roblx

--- src/App.jsx
+++ src/App.jsx
@@ -84,62 +84,53 @@ function App() {
             }));
             `}
         />
-      </div>
-    </>
-  );
-}
 
-export default App;
-    <div className='Container'>
-      <Card
-      Titulo="O que são props?"
-      desc="Props são dados passados de um componente para outro, permitindo que ele mostre informações diferentes."
-      code={`<Card titulo="Olá mundo" />`}
-      /> 
-      
+        <Card
+          Titulo="O que são props?"
+          desc="Props são dados passados de um componente para outro, permitindo que ele mostre informações diferentes."
+          code={`<Card titulo="Olá mundo" />`}
+        />
 
+        <Card
+          Titulo="useEffect sem dependências"
+          desc="Executa o efeito em toda renderização do componente."
+          code={`useEffect(() => {console.log("Executa em toda renderização")})`}
+        />
 
-      <Card
-      Titulo="useEffect sem dependências"
-      desc="Executa o efeito em toda renderização do componente."
-      code={`useEffect(() => {console.log("Executa em toda renderização")})`}
-      />
+        <Card
+          Titulo="useEffect com array vazio"
+          desc="Executa o efeito apenas uma vez, quando o componente é montado."
+          code={`useEffect(() => {console.log("Executa apenas uma vez")}, [])`}
+        />
 
-      <Card
-      Titulo="useEffect com array vazio"
-      desc="Executa o efeito apenas uma vez, quando o componente é montado."
-      code={`useEffect(() => {console.log("Executa apenas uma vez")}, [])`}
-      />
+        <Card
+          Titulo="useEffect com dependências"
+          desc="Executa o efeito quando o valor de count muda."
+          code={`useEffect(() => {console.log("Count mudou:", count)}, [count])`}
+        />
 
-      <Card
-      Titulo="useEffect com dependências"
-      desc="Executa o efeito quando o valor de count muda."
-      code={`useEffect(() => {console.log("Count mudou:", count)}, [count])`}
-      />
-
-      <Card
-      Titulo="useEffect com cleanup"
-      desc="Executa um efeito e limpa quando o componente é desmontado"
-      code={`useEffect(() => {const intervalo = setInterval(() => {console.log("Rodando);}, 1000);
+        <Card
+          Titulo="useEffect com cleanup"
+          desc="Executa um efeito e limpa quando o componente é desmontado"
+          code={`useEffect(() => {const intervalo = setInterval(() => {console.log("Rodando);}, 1000);
         return () => {clearInterval(intervalo);
         console.log("Limpou)};}, []);`}
-      />
+        />
 
-
-      <Card
-      Titulo="Ciclo de vida completo"
-      desc="Simula montagem, atualização e desmontagem com múltiplos useEffect."
-      code={`//Montagem
+        <Card
+          Titulo="Ciclo de vida completo"
+          desc="Simula montagem, atualização e desmontagem com múltiplos useEffect."
+          code={`//Montagem
       useEffect(() => {console.log("Componente montado")}, []);
       //Atualização
       useEffect(() => {console.log("Componente atualizado:", count);}, [count]);
       
       //Desmontagem
       useEffect(() => { return () => {console.log("Componente desmontado);};}, []);`}
-      />   
+        />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
