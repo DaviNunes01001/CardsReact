9b58b816e5243d6f6f102575abbb36e91c0920e8
Lucas <laercioemanuel1@gmail.com>
2026-03-24 09:59:31 -0300

    city boy1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,26 @@ function App() {
       Titulo="O que são props?"
       desc="Props são dados passados de um componente para outro, permitindo que ele mostre informações diferentes."
       code={`<Card titulo="Olá mundo" />`}
-      /> </div>
+      />
+      
+      <Card
+      Titulo="Renderização condicional"
+      desc="Permite mostrar ou esconder elementos na tela dependendo de uma condição. Muito usado com if, operador ternário (?) ou &&."
+      code={`{logado ? <Dashboard /> : <Login />}
+      
+        {isAdmin && <PainelAdmin />}`}
+      />
+      <Card
+      Titulo="Listas com map()"
+      desc="Usamos o map() para percorrer arrays e renderizar vários componentes dinamicamente."
+      code={`const nomes = ["Ana", "Carlos", "João"];
+
+      {no mes.map((nome) => (
+
+      <p>{nome}</p> ))}`} 
+      />
+
+</div>
     </>
   )
 }
